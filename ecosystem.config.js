4165979f1e25ae24916d4224187ea2ebc4ea47f0
XieLiang <liang.x@taboola.com>
2024-11-05 13:33:49 +0800

    watch: false

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: "./src/app.js", 
       instances: "max", // 实例数量，"max" 表示使用所有可用 CPU 核心
       exec_mode: "cluster", // 执行模式，可以是 "fork" 或 "cluster"
-      watch: true, // 启用文件监视（开发时可用）
+      watch: false, // 启用文件监视（开发时可用）
       env: {
         NODE_ENV: "development", // 开发环境变量
       },
