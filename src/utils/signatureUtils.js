42493d2d2003b1d7d94a227ee6111e5c099f3c9e
XieLiang <liang.x@taboola.com>
2024-11-07 16:34:02 +0800

    bug: fix sign

--- src/utils/signatureUtils.js
+++ src/utils/signatureUtils.js
@@ -3,7 +3,7 @@ const constants = require("../config");
 const apiSecret = constants.apiSecret;
 
 function generateSignature(source, regions, date) {
-  const stringToSign = `${apiSecret}${source}${regions}${date}`;
+  const stringToSign = `${apiSecret}${source}${JSON.stringify(regions)}${date}`;
   return crypto.createHash("md5").update(stringToSign).digest("hex");
 }
 module.exports = generateSignature;
